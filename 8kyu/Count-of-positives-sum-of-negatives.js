df9a1e5a42707fdf6a4f205d0ea7e31cc1cba8e8
Onyekachi Okwuolisa <kachiokwuolisa@gmail.com>
2022-05-10 15:40:58 +0100

    Completed Count of positive sum of negative

---
+++ 8kyu/Count-of-positives-sum-of-negatives.js
@@ -0,0 +1,40 @@
+// Given an array of integers.
+
+// Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+
+// If the input is an empty array or is null, return an empty array.
+
+// Example
+// For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+
+function countPositivesSumNegatives(input) {
+    let output = []
+    let positive= []
+    let negative = []
+    let sum = 0
+    if(input){
+      for(let num of input){
+        if(num > 0){
+          positive.push(num)
+        }else{
+          negative.push(num)
+          sum += num
+        }
+      }
+       if(positive.length >= 1 || negative.length >= 1){
+            output.push(positive.length, sum)
+        }
+    }
+    return output;
+  }
+
+
+  let testData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15];
+  let actual = countPositivesSumNegatives(testData);
+//   let expected = [10, -65];
+
+  let testData = [0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14];
+  let actual = countPositivesSumNegatives(testData);
+//   let expected = [8, -50];
+
+//LINK: https://www.codewars.com/kata/576bb71bbbcf0951d5000044/train/javascript
