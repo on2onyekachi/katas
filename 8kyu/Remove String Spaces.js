5b491ce95840bfa1918f22cf604ac284b504ce95
Onyekachi Okwuolisa <kachiokwuolisa@gmail.com>
2022-05-19 22:38:21 +0100

    Completed Remove String Spaces

---
+++ 8kyu/Remove String Spaces.js
@@ -0,0 +1,14 @@
+// Simple, remove the spaces from the string, then return the resultant string.
+
+
+function noSpace(x){
+    return x.split(' ').map(y => y.trim()).join('')
+  }
+
+
+noSpace('8 j 8   mBliB8g  imjB8B8  jl  B') // '8j8mBliB8gimjB8B8jlB');
+noSpace('8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd') // '88Bifk8hB8BB8BBBB888chl8BhBfd'); 
+noSpace('8aaaaa dddd r     ') // '8aaaaaddddr'); 
+
+
+//LINK: https://www.codewars.com/kata/57eae20f5500ad98e50002c5/solutions/javascript
