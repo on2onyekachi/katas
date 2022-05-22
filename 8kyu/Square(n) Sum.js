20663e1b271420746fce5a3c150af2b37d6cf6ee
Onyekachi Okwuolisa <kachiokwuolisa@gmail.com>
2022-05-22 18:08:22 +0100

    Completed Square(n) Sum

---
+++ 8kyu/Square(n) Sum.js
@@ -0,0 +1,15 @@
+// Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+// For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
+
+function squareSum(numbers){
+    return numbers.reduce((acc, inc) => acc += inc*inc, 0)
+  }
+
+
+squareSum([1,2]) // 5
+squareSum([0, 3, 4, 5]) // 50
+squareSum([]) // 0
+
+
+//LINK: https://www.codewars.com/kata/515e271a311df0350d00000f/train/javascript
