9b639bdca389c4ff65e66b5403489c31f1b609b2
Onyekachi Okwuolisa <kachiokwuolisa@gmail.com>
2022-06-12 20:43:08 +0100

    Completed Function 3 - multiplying two numbers

---
+++ 8kyu/Function 3 - multiplying two numbers.js
@@ -0,0 +1,11 @@
+// Implement a function which multiplies two numbers.
+
+// Write here your multiply-function
+function multiply(x,y){
+    return x*y
+  }
+
+
+multiply(2, 3)// 6
+
+//LINK: https://www.codewars.com/kata/523b66342d0c301ae400003b/train/javascript
