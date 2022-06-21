37df8f33c8550606b21116e122364e750a3f3dbe
Onyekachi Okwuolisa <kachiokwuolisa@gmail.com>
2022-06-21 06:37:14 +0100

    Completed Grasshopper - Messi Goals

---
+++ 8kyu/Grasshopper - Messi Goals.js
@@ -0,0 +1,31 @@
+// Messi's Goal Total
+// Use variables to find the sum of the goals Messi scored in 3 competitions
+
+// Information
+// Messi goal scoring statistics:
+
+// Competition	Goals
+// La Liga	43
+// Champions League	10
+// Copa del Rey	5
+// Task
+// Create these three variables and store the appropriate values using the table above:
+// laLigaGoals
+// championsLeagueGoals
+// copaDelReyGoals
+// Create a fourth variable named totalGoals that stores the sum of all of Messi's goals for this year.
+
+var laLigaGoals;
+var championsLeagueGoals;
+var copaDelReyGoals;
+
+var totalGoals;
+laLigaGoals = 43;
+championsLeagueGoals = 10;
+copaDelReyGoals = 5;
+totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals;
+
+totalGoals //  58
+
+
+//LINK: https://www.codewars.com/kata/55ca77fa094a2af31f00002a/train/javascript
