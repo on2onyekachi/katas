fb4a19195f1ae926152851ae60a0046058709959
Onyekachi Okwuolisa <kachiokwuolisa@gmail.com>
2022-07-08 21:43:43 +0100

    Completed Function 2 - squaring an argument

---
+++ 8kyu/Function 2 - squaring an argument.js
@@ -0,0 +1,8 @@
+// Now you have to write a function that takes an argument and returns the square of it.
+
+
+const square = (n) => n*n
+
+square( 3 ) // 9
+
+//link: https://www.codewars.com/kata/523b623152af8a30c6000027/solutions/javascript
