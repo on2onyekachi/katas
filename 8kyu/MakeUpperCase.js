4bb237b4d7ada0cfc84b5aa012d7545829deaec8
Onyekachi Okwuolisa <kachiokwuolisa@gmail.com>
2022-07-15 01:45:28 +0100

    Completed MakeUpperCase

---
+++ 8kyu/MakeUpperCase.js
@@ -0,0 +1,16 @@
+// Write a function which converts the input string to uppercase.
+
+function makeUpperCase(str) {
+    // Code here
+    return str.toUpperCase()
+  }
+
+
+makeUpperCase("hello") // "HELLO"
+makeUpperCase("hello world") //  "HELLO WORLD"
+makeUpperCase("hello world !") // "HELLO WORLD !"
+makeUpperCase("heLlO wORLd !") // "HELLO WORLD !"
+makeUpperCase("1,2,3 hello world!") // "1,2,3 HELLO WORLD!"
+
+
+//LLINK: https://www.codewars.com/kata/57a0556c7cb1f31ab3000ad7/solutions/javascript
