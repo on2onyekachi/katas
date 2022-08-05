9dc1a7f29599e3ae49b8c65c1dcc790f4aa72f2d
Onyekachi Okwuolisa <kachiokwuolisa@gmail.com>
2022-08-05 17:38:42 +0100

    CSV representation of array

---
+++ 8kyu/CSV representation of array.js
@@ -0,0 +1,46 @@
+// Create a function that returns the CSV representation of a two-dimensional numeric array.
+
+// Example:
+
+// input:
+//    [[ 0, 1, 2, 3, 4 ],
+//     [ 10,11,12,13,14 ],
+//     [ 20,21,22,23,24 ],
+//     [ 30,31,32,33,34 ]] 
+    
+// output:
+//      '0,1,2,3,4\n'
+//     +'10,11,12,13,14\n'
+//     +'20,21,22,23,24\n'
+//     +'30,31,32,33,34'
+// Array's length > 2.
+
+// More details here: https://en.wikipedia.org/wiki/Comma-separated_values
+
+function toCsvText(array) {
+    // good luck
+   return array.map(x => x.join(',')).join('\n')
+ }
+
+
+
+ toCsvText([
+    [ 0, 1, 2, 3, 45 ],
+    [ 10,11,12,13,14 ],
+    [ 20,21,22,23,24 ],
+    [ 30,31,32,33,34 ]
+   ] ) //  '0,1,2,3,45\n10,11,12,13,14\n20,21,22,23,24\n30,31,32,33,34');
+   
+toCsvText([
+    [ -25, 21, 2, -33, 48 ],
+    [ 30,31,-32,33,-34 ]
+   ] ) //  '-25,21,2,-33,48\n30,31,-32,33,-34');
+   
+toCsvText([
+    [ 5,55,5,5,55 ],
+    [ 6,6,66,23,24 ],
+    [ 666,31,66,33,7 ]
+   ] ) // '5,55,5,5,55\n6,6,66,23,24\n666,31,66,33,7');
+
+
+   //link: https://www.codewars.com/kata/5a34af40e1ce0eb1f5000036/solutions/javascript
