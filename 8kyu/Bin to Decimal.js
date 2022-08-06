28125fa87cacbb2edf7a08ee6ce594daf651c41f
Onyekachi Okwuolisa <kachiokwuolisa@gmail.com>
2022-08-06 17:53:27 +0100

    Bin to Decimal

---
+++ 8kyu/Bin to Decimal.js
@@ -0,0 +1,10 @@
+// Complete the function which converts a binary number (given as a string) to a decimal number.
+
+function binToDec(bin){
+    // ...
+    return parseInt(bin, 2)
+  }
+
+// ["1",1], ["0",0], ["1001001", 73]
+
+//LINK: https://www.codewars.com/kata/57a5c31ce298a7e6b7000334/train/javascript
