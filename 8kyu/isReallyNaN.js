4d86d12db57395c8843eb38a5077febedca4144c
Onyekachi Okwuolisa <kachiokwuolisa@gmail.com>
2022-08-26 16:14:54 +0100

    isReallyNaN

---
+++ 8kyu/isReallyNaN.js
@@ -0,0 +1,20 @@
+// I've hit a few bugs in my Java/Type/Coffee-script code recently, and I've traced the problem back to the global isNaN function I was using. I had expected it to be more discerning, but it's returning true for undefined right now.
+
+// Write a function isReallyNaN that returns true only if passed in an argument that evalutes to NaN, and returns false otherwise.
+
+// Any solution is acceptable!
+
+// FUNDAMENTALS
+
+const isReallyNaN = (val) => {
+  
+    return Number.isNaN(val);
+  };
+
+  isReallyNaN(0) // false
+isReallyNaN(37) // false
+isReallyNaN('37') // false
+isReallyNaN(NaN) // true
+isReallyNaN(undefined) // false
+
+//link: https://www.codewars.com/kata/56c24c58e0c0f741d4001aef/solutions/javascript?filter=all&sort=best_practice&invalids=false
