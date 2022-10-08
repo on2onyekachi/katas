033f2af47c8558d17dd268775652766e9e2d5ff5
Onyekachi Okwuolisa <kachiokwuolisa@gmail.com>
2022-10-08 13:09:38 +0100

    How many lightsabers do you own

---
+++ 8kyu/How many lightsabers do you own?.js
@@ -0,0 +1,22 @@
+// Inspired by the development team at Vooza, write the function that
+
+// accepts the name of a programmer, and
+// returns the number of lightsabers owned by that person.
+// The only person who owns lightsabers is Zach, by the way. He owns 18, which is an awesome number of lightsabers. Anyone else owns 0.
+
+// Note: your function should have a default parameter.
+
+// For example(Input --> Output):
+
+// "anyone else" --> 0
+// "Zach" --> 18
+
+function howManyLightsabersDoYouOwn(name) {
+    return name && name.toLowerCase() == 'zach' ? 18 : 0
+  }
+
+howManyLightsabersDoYouOwn() // 0
+howManyLightsabersDoYouOwn("Adam") // 0
+howManyLightsabersDoYouOwn("Zach") // 18
+
+  //LLINK: https://www.codewars.com/kata/51f9d93b4095e0a7200001b8/train/javascript
